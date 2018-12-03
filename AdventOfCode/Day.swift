db5a2de179f4f34aab84a69ed2f658fe77b0a8c5
nevillco <connor.neville@raizlabs.com>
2018-12-03 08:58:19 -0500

    Add Day protocol for reusable code, add function to get input file

---
+++ AdventOfCode/Day.swift
@@ -0,0 +1,31 @@
+//
+//  Day.swift
+//  AdventOfCode
+//
+//  Created by Connor Neville on 12/3/18.
+//  Copyright © 2018 Connor Neville. All rights reserved.
+//
+
+import Foundation
+
+protocol Day {
+
+}
+
+extension Day {
+
+    func getInput() -> String {
+        guard let path = Bundle.main.path(forResource: String(describing: type(of: self)), ofType: "txt") else {
+            fatalError("Input text file not found in bundle")
+        }
+        let contents: String
+        do {
+            contents = try String(contentsOfFile: path)
+        }
+        catch {
+            fatalError("Unable to parse input file: \(error.localizedDescription)")
+        }
+        return contents
+    }
+
+}
--- AdventOfCode/Day1/Day1.swift
+++ AdventOfCode/Day1/Day1.swift
@@ -1,9 +1,13 @@
 //
-//  main.swift
+//  Day1.swift
 //  AdventOfCode
 //
 //  Created by Connor Neville on 12/2/18.
 //  Copyright © 2018 Connor Neville. All rights reserved.
 //
 
-print(Day1().getInput())
+import Foundation
+
+final class Day1: Day {
+
+}
