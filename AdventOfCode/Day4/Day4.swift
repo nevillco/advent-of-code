f7c68bb07c1d605f6a95add20e1e6115d3bed9e5
nevillco <connor.neville@raizlabs.com>
2018-12-03 23:35:14 -0500

    Add files for day 4

---
+++ AdventOfCode/Day4/Day4.swift
@@ -0,0 +1,21 @@
+//
+//  Day4.swift
+//  AdventOfCode
+//
+//  Created by Connor Neville on 12/3/18.
+//  Copyright © 2018 Connor Neville. All rights reserved.
+//
+
+import Foundation
+
+final class Day4: Day {
+
+    func part1() -> String {
+        return ""
+    }
+
+    func part2() -> String {
+        return ""
+    }
+
+}
