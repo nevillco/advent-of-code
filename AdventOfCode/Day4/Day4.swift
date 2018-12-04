925df95d41981d78758d7183035818a3d192857c
nevillco <connor.neville@raizlabs.com>
2018-12-04 00:25:38 -0500

    Get parsing/sorting working for day 1 part 1

--- AdventOfCode/Day4/Day4.swift
+++ AdventOfCode/Day4/Day4.swift
@@ -10,7 +10,52 @@ import Foundation
 
 final class Day4: Day {
 
+    enum EntryType {
+        case guardStarting(id: Int)
+        case fallsAsleep
+        case wakesUp
+
+        init(substring: String) {
+            let keyword = substring.split(separator: " ")[1]
+            switch keyword {
+            case "up":
+                self = .wakesUp
+            case "asleep":
+                self = .fallsAsleep
+            default:
+                let number = Int(keyword.dropFirst())!
+                self = .guardStarting(id: number)
+            }
+        }
+    }
+
+    struct Entry {
+        let date: Date
+        let type: EntryType
+
+        static var formatter: DateFormatter = {
+            let f = DateFormatter()
+            f.dateFormat = "yyyy-MM-dd HH:mm"
+            return f
+        }()
+
+        init(line: String) {
+            let dateStart = line.index(line.startIndex, offsetBy: 1)
+            let dateEnd = line.firstIndex(of: "]")!
+            let dateString = String(line[dateStart..<dateEnd])
+            date = Entry.formatter.date(from: dateString)!
+            let typeString = String(line[line.index(dateEnd, offsetBy: 2)...])
+            type = .init(substring: typeString)
+        }
+    }
+    lazy var entries: [Entry] = {
+        return self.lines()
+            .map({ Entry(line: $0) })
+            .sorted(by: { (e1, e2) in e1.date < e2.date })
+    }()
+
     func part1() -> String {
+        print(entries.map({ $0.type}))
         return ""
     }
 
