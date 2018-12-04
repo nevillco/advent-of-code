6b984e344b42e34ed1d7f33344c836763ec6c2dd
nevillco <connor.neville@raizlabs.com>
2018-12-04 01:02:02 -0500

    Solve day 4

--- AdventOfCode/Day4/Day4.swift
+++ AdventOfCode/Day4/Day4.swift
@@ -53,14 +53,67 @@ final class Day4: Day {
             .map({ Entry(line: $0) })
             .sorted(by: { (e1, e2) in e1.date < e2.date })
     }()
+    lazy var sleepRanges: [Int: [Range<Int>]] = {
+        var sleepRanges: [Int: [Range<Int>]] = [:]
+        var activeGuardID: Int!
+        var lastTimestamp: Date!
+        for entry in entries {
+            switch entry.type {
+            case .guardStarting(let id):
+                activeGuardID = id
+            case .fallsAsleep:
+                lastTimestamp = entry.date
+            case .wakesUp:
+                let sleepMinute = Calendar.current.component(.minute, from: lastTimestamp)
+                let wakeMinute = Calendar.current.component(.minute, from: entry.date)
+                let range = [sleepMinute..<wakeMinute]
+                sleepRanges[activeGuardID, default: []].append(contentsOf: range)
+            }
+        }
+        return sleepRanges
+    }()
 
     func part1() -> String {
-        print(entries.map({ $0.type}))
-        return ""
+        let timeAsleep = sleepRanges.mapValues { ranges in
+            return ranges.reduce(into: 0, { (result, range) in
+                result += (range.endIndex - range.startIndex)
+            })
+        }
+        let maxTimeAsleep = timeAsleep.max(by: { (tuple1, tuple2) -> Bool in
+            return tuple1.value < tuple2.value
+        })!
+        let guardID = maxTimeAsleep.key
+        let checksum = guardID * mostCommonMinuteAsleep(guardID: guardID).minute
+        return String(describing: checksum)
     }
 
     func part2() -> String {
-        return ""
+        let maxFrequencies: [Int: MinuteFrequency] = sleepRanges.reduce(into: [:]) { (result, tuple) in
+            result[tuple.key] = mostCommonMinuteAsleep(guardID: tuple.key)
+        }
+        let winner = maxFrequencies.max(by: { (tuple1, tuple2) -> Bool in
+            return tuple1.value.frequency < tuple2.value.frequency
+        })!
+        let checksum = winner.key * winner.value.minute
+        print(winner)
+        return String(describing: checksum)
+    }
+
+    func sleepFrequenciesByMinute(guardID: Int) -> [Int: Int] {
+        return (0..<59).reduce(into: [Int: Int](), { (result, next) in
+            let frequencies = sleepRanges[guardID]!.filter({ range in
+                return range.contains(next)
+            })
+            result[next] = frequencies.count
+        })
+    }
+
+    typealias MinuteFrequency = (minute: Int, frequency: Int)
+    func mostCommonMinuteAsleep(guardID: Int) -> MinuteFrequency {
+        let result = sleepFrequenciesByMinute(guardID: guardID).max(by: { tuple1, tuple2 -> Bool in
+            return tuple1.value < tuple2.value
+        })
+        return (result!.key, result!.value)
     }
 
 }
