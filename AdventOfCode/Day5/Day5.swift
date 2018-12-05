03a91e1eb96c83ddc96bdd23540a87b1990a58f9
nevillco <connor.neville@raizlabs.com>
2018-12-05 09:33:03 -0500

    Solve Day5 part1

--- AdventOfCode/Day5/Day5.swift
+++ AdventOfCode/Day5/Day5.swift
@@ -10,4 +10,32 @@ import Foundation
 
 final class Day5: Day {
 
+    lazy var input: String = { return self.input().trimmingCharacters(in: .newlines) }()
+
+    func part1() -> String {
+        var counter = 0
+        var output = input
+        while counter < output.count - 1 {
+            let currentIndex = output.index(input.startIndex, offsetBy: counter)
+            let currentChar = output[currentIndex]
+            let nextIndex = output.index(input.startIndex, offsetBy: counter + 1)
+            let nextChar = output[nextIndex]
+            if canReact(currentChar, nextChar) {
+                let subRange = currentIndex...nextIndex
+                output.removeSubrange(subRange)
+                counter = max(counter - 1, 0)
+            }
+            else {
+                counter += 1
+            }
+        }
+        return String(describing: output.count)
+    }
+
+    func canReact(_ char1: Character, _ char2: Character) -> Bool {
+        let string1 = String(char1)
+        let string2 = String(char2)
+        return string1 != string2 && string1.uppercased() == string2.uppercased()
+    }
+
 }
