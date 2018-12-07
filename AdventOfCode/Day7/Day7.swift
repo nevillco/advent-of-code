d3025f8cfc3feae7b8d9d17058ce454a8e4ad39b
nevillco <connor.neville@raizlabs.com>
2018-12-07 09:14:58 -0500

    Solve day 7 part 1

--- AdventOfCode/Day7/Day7.swift
+++ AdventOfCode/Day7/Day7.swift
@@ -10,4 +10,56 @@ import Foundation
 
 final class Day7: Day {
 
+    struct StepRequirement {
+        let required: String
+        let enabling: String
+
+        init(line: String) {
+            let separated = line.split(separator: " ")
+            required = String(separated[1])
+            enabling = String(separated[7])
+        }
+    }
+    lazy var stepRequirements: [StepRequirement] = {
+        return lines().map { StepRequirement(line: $0) }.sorted { $0.required < $1.required }
+    }()
+    lazy var allSteps: [String] = {
+        return Array(Set(stepRequirements.map { $0.required } + stepRequirements.map { $0.enabling }))
+    }()
+
+    func part1() -> String {
+        var requirements = stepRequirements
+        var completed: [String] = []
+        while completed.count < allSteps.count {
+            let next = nextStep(with: requirements, completed: completed)
+            completed.append(next)
+            requirements = requirements.filtering(step: next)
+        }
+        return completed.joined()
+    }
+
+    func enabledSteps(with requirements: [StepRequirement], completed: [String]) -> [String] {
+        let isEnabled: (String) -> Bool = { string in
+            return !requirements.contains(where: { requirement in
+                requirement.enabling == string
+            })
+        }
+        return (0..<26)
+            .map({ String(Character(UnicodeScalar($0 + 65))) })
+            .filter(isEnabled)
+            .filter { !completed.contains($0) }
+    }
+
+    func nextStep(with requirements: [StepRequirement], completed: [String]) -> String {
+        return enabledSteps(with: requirements, completed: completed).first!
+    }
+
+}
+
+extension Array where Element == Day7.StepRequirement {
+
+    func filtering(step: String) -> [Day7.StepRequirement] {
+        return filter { $0.required != step }
+    }
+
 }
