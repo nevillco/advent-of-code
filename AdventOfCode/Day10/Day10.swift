20d99c7ef0c6ed8d0ff6f434e6d16a72f712b11e
nevillco <connor.neville@raizlabs.com>
2018-12-10 10:32:19 -0500

    Do parsing for day10

--- AdventOfCode/Day10/Day10.swift
+++ AdventOfCode/Day10/Day10.swift
@@ -9,5 +9,30 @@
 import Foundation
 
 final class Day10: Day {
-    
+
+    struct Vector {
+        let position: AnyPosition
+        let velocity: AnyPosition
+
+        init(line: String) {
+            let indices: ((Character) -> String.Index?) -> ClosedRange<String.Index> = { function in
+                return line.index(after: function("<")!)...line.index(before: function(">")!)
+            }
+            let values: (Substring) -> [Int] = { substring in
+                return substring.split(separator: ",")
+                    .map { Int($0.trimmingCharacters(in: .whitespaces))! }
+            }
+            let firstPair = values(line[indices(line.firstIndex)])
+            position = .init(x: firstPair[0], y: firstPair[1])
+            let secondPair = values(line[indices(line.lastIndex)])
+            velocity = .init(x: secondPair[0], y: secondPair[1])
+        }
+    }
+    lazy var vectors: [Vector] = { lines().map { .init(line: $0) } }()
+
+    func part1() -> String {
+        print(vectors)
+        return ""
+    }
+
 }
